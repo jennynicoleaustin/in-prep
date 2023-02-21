6d12c459df19e4f0ac7bc3bdbc9fd3a1657e810e
Jenny Austin <jennynicoleaustin@gmail.com>
2023-02-21 11:32:00 -0600

    Section 18 -> V.238 Attaching Payloads to Actions

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -6,24 +6,29 @@ const Counter = () => {
     const counter = useSelector(state => state.counter);
 
     const incrementHandler = () => {
-        dispatch({ type: 'increment'})
+        dispatch({ type: 'increment' })
+    }
+    const increaseHandler = () => {
+        dispatch({ type: 'increase', amount: 5 })
     }
     const decrementHandler = () => {
-        dispatch({ type: 'decrement' })
+        dispatch({type: 'decrement'})
     }
-  const toggleCounterHandler = () => {};
+    const toggleCounterHandler = () => {
+    };
 
-  return (
-    <main className={classes.counter}>
-      <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
-        <div>
-            <button onClick={incrementHandler}>Increment</button>
-            <button onClick={decrementHandler}>Decrement</button>
-        </div>
-      <button onClick={toggleCounterHandler}>Toggle Counter</button>
-    </main>
-  );
+    return (
+        <main className={classes.counter}>
+            <h1>Redux Counter</h1>
+            <div className={classes.value}>{counter}</div>
+            <div>
+                <button onClick={incrementHandler}>Increment</button>
+                <button onClick={decrementHandler}>Decrement</button>
+                <button onClick={increaseHandler}>Increase by 5</button>
+            </div>
+            <button onClick={toggleCounterHandler}>Toggle Counter</button>
+        </main>
+    );
 };
 
 export default Counter;
