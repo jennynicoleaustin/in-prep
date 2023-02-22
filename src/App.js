cf8539a8dc93d213f03e23495f43672d8ca5fa48
Jenny Austin <jennynicoleaustin@gmail.com>
2023-02-22 16:57:35 -0600

    Section 18 -> V.246 Reading & Dispatching From A New Slice

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import Counter from './components/Counter';
 import Header from "./components/Header";
 import Auth from "./components/Auth";
+import {useSelector} from "react-redux";
+import UserProfile from "./components/UserProfile";
 
 
 function App() {
-  return (
-      <>
-        <Header />
-        <Auth />
-        <Counter />
-      </>
 
-  );
+    const isAuthenticated = useSelector(state => state.auth.isAuthenticated)
+
+    return (
+        <>
+            <Header/>
+            {!isAuthenticated && <Auth/>}
+            {isAuthenticated && <UserProfile/>}
+            <Counter/>
+        </>
+
+    );
 }
 
 export default App;
