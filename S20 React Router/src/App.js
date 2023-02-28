7bd15a8e4cd2658f1f546a5c8a052fbe20ec9083
Jenny Austin <jennynicoleaustin@gmail.com>
2023-02-28 17:29:36 -0600

    Section 20 - V6 React Router v.275 - Difference between Link & NavLink

--- S20 React Router/src/App.js
+++ S20 React Router/src/App.js
@@ -1,5 +1,34 @@
+
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<HomePage />} />
+//     <Route path="/products" element={<ProductsPage />} />
+//   </Route>
+// );
+
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import RootLayout from "./Pages/Root";
+import ErrorPage from "./Pages/Error";
+import HomePage from "./Pages/Home";
+import ProductsPage from "./Pages/ProductsPage";
+
+// by defining children paths, you can have route dependent layouts!
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { path: '/', element: <HomePage /> },
+      { path: '/products', element: <ProductsPage /> },
+    ],
+  }
+]);
+
+// const router = createBrowserRouter(routeDefinitions);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
