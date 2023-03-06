2ad54f834e4b1a2b208fec9ca5b170448f19e992
Jenny Austin <jennynicoleaustin@gmail.com>
2023-03-06 13:40:35 -0600

    Section 20 - V6 React Router v.279 absolute and relative paths

--- S20 React Router/src/App.js
+++ S20 React Router/src/App.js
@@ -21,8 +21,8 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
     children: [
       { path: '/', element: <HomePage /> },
-      { path: '/products', element: <ProductsPage /> },
-      { path: '/products/:productId', element: <ProductDetailPage /> },
+      { path: 'products', element: <ProductsPage /> },
+      { path: 'products/:productId', element: <ProductDetailPage /> },
     ],
   }
 ]);
