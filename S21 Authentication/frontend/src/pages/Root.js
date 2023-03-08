2c1b447fa3d725f6d8039349fedc434dad9dd8dd
Jenny Austin <jennynicoleaustin@gmail.com>
2023-03-08 16:08:50 -0600

    Section 21: Authentication V. 322 Adding Automatic Logout

--- S21 Authentication/frontend/src/pages/Root.js
+++ S21 Authentication/frontend/src/pages/Root.js
@@ -1,19 +1,30 @@
-import { Outlet, useNavigation } from 'react-router-dom';
+import {Outlet, useLoaderData, useNavigation, useSubmit} from 'react-router-dom';
 
 import MainNavigation from '../components/MainNavigation';
+import {useEffect} from "react";
 
 function RootLayout() {
-  // const navigation = useNavigation();
+    const token = useLoaderData();
+    const submit = useSubmit();
 
-  return (
-    <>
-      <MainNavigation />
-      <main>
-        {/* {navigation.state === 'loading' && <p>Loading...</p>} */}
-        <Outlet />
-      </main>
-    </>
-  );
+    useEffect(() => {
+        if (!token) {
+            return;
+        }
+
+        setTimeout(() => {
+            submit(null, {action: '/logout', method: 'post'})
+        }, 60 * 60 * 1000)
+    }, [token, submit])
+
+    return (
+        <>
+            <MainNavigation/>
+            <main>
+                <Outlet/>
+            </main>
+        </>
+    );
 }
 
 export default RootLayout;
