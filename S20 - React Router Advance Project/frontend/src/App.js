6b8476bd002a918198b46f10ba742cd745e72338
Jenny Austin <jennynicoleaustin@gmail.com>
2023-03-08 12:19:31 -0600

    Section 20: React Router Dom -> V.302 Reusing Actions via Request Methods

--- S20 - React Router Advance Project/frontend/src/App.js
+++ S20 - React Router Advance Project/frontend/src/App.js
@@ -3,13 +3,14 @@ import HomePage from "./pages/HomePage"
 import RootLayout from "./pages/RootLayout";
 import EventsPage, {loader as eventsLoader} from "./pages/Events";
 import EditEventPage from "./pages/EditEvent";
-import NewEventPage, { action as newEventAction } from "./pages/NewEvent";
+import NewEventPage from "./pages/NewEvent";
 import EventDetailPage, {
     loader as eventDetailLoader,
     action as deleteEventAction
 } from "./pages/EventDetail";
 import EventsRootLayout from "./pages/EventsRootLayout";
 import ErrorPage from "./pages/ErrorPage";
+import { action as manipulateEventAction } from "./components/EventForm"
 
 const router = createBrowserRouter([
     {
@@ -39,12 +40,17 @@ const router = createBrowserRouter([
                             },
                             {
                                 path: 'edit',
-                                element: <EditEventPage/>
+                                element: <EditEventPage/>,
+                                action: manipulateEventAction
                             },
                         ]
                     },
 
-                    {path: 'new', element: <NewEventPage/>, action: newEventAction },
+                    {
+                        path: 'new',
+                        element: <NewEventPage/>,
+                        action: manipulateEventAction
+                    },
 
                 ],
             },
